ad48b5bd7a985919b182f280c3b4f6b78d329d4e
pradeep <pbpradeeptechno@gmail.com>
2019-04-10 17:05:39 +0530

    auth gaurd added and create post force share done

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,22 +1,26 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AuthGuard } from './services/user/auth.guard';
 
 const routes: Routes = [
-  { path: '', redirectTo: 'landing', pathMatch: 'full' },
-  { path: 'home', loadChildren: './home/home.module#HomePageModule' },
-  { path: 'login', loadChildren: './login/login.module#LoginPageModule' },
-  { path: 'register', loadChildren: './register/register.module#RegisterPageModule' },
-  { path: 'listing', loadChildren: './listing/listing.module#ListingPageModule' },
-  { path: 'create-post', loadChildren: './create-post/create-post.module#CreatePostPageModule' },
-  { path: 'post-view/:id', loadChildren: './post-view/post-view.module#PostViewPageModule' },
-  { path: 'landing', loadChildren: './landing/landing.module#LandingPageModule' },
-  { path: 'edit-post/:id', loadChildren: './edit-post/edit-post.module#EditPostPageModule' },  { path: 'terms', loadChildren: './terms/terms.module#TermsPageModule' },
-  { path: 'message', loadChildren: './message/message.module#MessagePageModule' },
+  { path: '',canActivate: [AuthGuard], redirectTo: 'listing', pathMatch: 'full' },
+  { path: 'home', canActivate: [AuthGuard],loadChildren: './home/home.module#HomePageModule' },
+  { path: 'login',loadChildren: './login/login.module#LoginPageModule' },
+  { path: 'register',loadChildren: './register/register.module#RegisterPageModule' },
+  { path: 'listing', canActivate: [AuthGuard],loadChildren: './listing/listing.module#ListingPageModule' },
+  { path: 'create-post', canActivate: [AuthGuard],loadChildren: './create-post/create-post.module#CreatePostPageModule' },
+  { path: 'post-view/:id', canActivate: [AuthGuard],loadChildren: './post-view/post-view.module#PostViewPageModule' },
+  { path: 'landing',loadChildren: './landing/landing.module#LandingPageModule' },
+  { path: 'edit-post/:id', canActivate: [AuthGuard],loadChildren: './edit-post/edit-post.module#EditPostPageModule' },
+  { path: 'terms', canActivate: [AuthGuard],loadChildren: './terms/terms.module#TermsPageModule' },
+  { path: 'message', canActivate: [AuthGuard],loadChildren: './message/message.module#MessagePageModule' },
 
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
+  providers: [AuthGuard]
 })
 export class AppRoutingModule { }
+
--- src/app/landing/landing.ts
+++ src/app/landing/landing.ts
@@ -1,12 +1,16 @@
 import { Component, OnInit } from '@angular/core';
 import { ApiService } from '../services/api/api.service';
+import { Router } from '@angular/router';
 @Component({
   selector: 'page-landing',
   templateUrl: 'landing.html',
 })
 export class LandingPage {
 	userCnt = 0;
-  constructor(private apiService: ApiService) {
+  constructor(private apiService: ApiService, public router: Router) {
+    // if(this.apiService.getSessionData()){
+    //   this.router.navigate(['/listing']);
+    // }
   	this.apiService.gettotaluser().subscribe(res => {
   		this.userCnt=res['data'];
   	});
